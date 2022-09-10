f45d269a33b999c3d9a67c8d82f5d86fb75be390
staceyjoon <checkhovaa@gmail.com>
2022-09-10 11:53:25 +0300

    Icon weather changes

--- index.js
+++ index.js
@@ -77,6 +77,7 @@ function showTemperature(response) {
   let humidity = response.data.main.humidity;
   let wind = Math.round(response.data.wind.speed);
   let condition = response.data.weather[0].main;
+  let iconElement =
   document.querySelector("#condition").innerHTML = condition;
   document.querySelector("#humidity").innerHTML = humidity;
   document.querySelector("#wind").innerHTML = wind;
